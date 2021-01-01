caaf2fe8ed1122bfc7d4efefec2b214837e24fe0
nguyenhoanganhvu <nguyenhoanganhvu.it@gmail.com>
2021-01-01 09:05:07 +0700

    create base form

--- DemoGithub/DemoGithub/Infor.Designer.cs
+++ DemoGithub/DemoGithub/Infor.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace DemoGithub
 {
-    partial class Form1
+    partial class Infor
     {
         /// <summary>
         /// Required designer variable.
@@ -29,33 +29,12 @@ namespace DemoGithub
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(280, 248);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 390);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Infor";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
-
